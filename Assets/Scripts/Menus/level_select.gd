3d80a8e7510f174b947d09a2c9737d1c05d4c7f9
Jan Heemstra <heemstrajan@gmail.com>
2016-04-15 21:16:46 +0200

    did more work on the player

--- Assets/Scripts/Menus/level_select.gd
+++ Assets/Scripts/Menus/level_select.gd
@@ -2,3 +2,6 @@ extends VBoxContainer
 
 func _on_Level1_pressed():
 	get_tree().change_scene("res://Levels/level1.scn")
+
+func _on_Back_pressed():
+	get_tree().change_scene("res://Levels/Menus/main_menu.scn")
