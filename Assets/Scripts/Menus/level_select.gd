0e25c30536e14cba233f29599a00cb0ade13cdff
Jan Heemstra <heemstrajan@gmail.com>
2016-04-15 21:27:56 +0200

    menus are now auto-focuced

--- Assets/Scripts/Menus/level_select.gd
+++ Assets/Scripts/Menus/level_select.gd
@@ -1,5 +1,8 @@
 extends VBoxContainer
 
+func _ready():
+	get_node("Level1").grab_focus()
+
 func _on_Level1_pressed():
 	get_tree().change_scene("res://Levels/level1.scn")
 
--- Assets/Scripts/Menus/main_menu.gd
+++ Assets/Scripts/Menus/main_menu.gd
@@ -1,5 +1,8 @@
 extends VBoxContainer
 
+func _ready():
+	get_node("Start").grab_focus()
+
 func _on_Start_pressed():
 	get_tree().change_scene("res://Levels/Menus/level_select.scn")
 
