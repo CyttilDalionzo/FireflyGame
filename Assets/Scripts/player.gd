4a411ab4ca5363900cc8cdf091ca43d7a712cb96
Jan Heemstra <heemstrajan@gmail.com>
2016-04-16 17:25:59 +0200

    implemented wall jumps

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -1,39 +1,64 @@
 extends RigidBody2D
 
-var jump_height = 200
-var run_speed = 220
-var strafe_force = 2
+var jump_height = 350
+var run_speed = 300
+var strafe_force = 4
 
-func is_colliding():
+func are_feet_colliding():
 	return get_node("Feet").get_overlapping_bodies().size() > 0
 
+func is_left_colliding():
+	return get_node("Left").get_overlapping_bodies().size() > 0
+
+func is_right_colliding():
+	return get_node("Right").get_overlapping_bodies().size() > 0
 
 func _ready():
-	#set_process_input(true)
+	set_process_input(true)
 	set_fixed_process(true)
 	set_mode(MODE_CHARACTER)
 	
 	set_contact_monitor(true)
 	set_max_contacts_reported(1)
-	
-	# Ignore collisions with ourselves
-	#get_node("Feet").add_exception(self)
 
 func _fixed_process(delta):
 	set_linear_damp(0.5)
 	
-	if !is_colliding():
+	if are_feet_colliding():
+		floory_controls(delta)
+	else:
 		floaty_controls(delta)
+
+func _input(event):
+	if event.is_action_pressed("jump") and !event.is_echo():
+		if are_feet_colliding():
+			ground_jump()
+		elif is_left_colliding():
+			left_wall_jump()
+		elif is_right_colliding():
+			right_wall_jump()
+
+func set_view_direction(is_left = null):
+	if is_left == null:
+		if get_linear_velocity().x < 0:
+			get_node("Sprite").set_scale(0.4 * Vector2(-1, 1))
+		else:
+			get_node("Sprite").set_scale(0.4 * Vector2(1, 1))
 		return
 	
-	if Input.is_action_pressed("jump"):
-		set_linear_velocity(Vector2(get_linear_velocity().x, -jump_height))
-	elif Input.is_action_pressed("ui_left"):
-		set_linear_velocity(Vector2(-run_speed, get_linear_velocity().y))
-	elif Input.is_action_pressed("ui_right"):
-		set_linear_velocity(Vector2(run_speed, get_linear_velocity().y))
+	if is_left:
+		get_node("Sprite").set_scale(0.4 * Vector2(1, 1))
 	else:
-		set_linear_damp(10.0)
+		get_node("Sprite").set_scale(0.4 * Vector2(-1, 1))
+
+func ground_jump():
+	set_linear_velocity( Vector2(get_linear_velocity().x, -jump_height) )
+
+func left_wall_jump():
+	set_linear_velocity( 0.7 * Vector2(jump_height, -jump_height * 1.2) )
+
+func right_wall_jump():
+	set_linear_velocity( 0.7 * Vector2(-jump_height, -jump_height * 1.2) )
 
 func floaty_controls(delta):
 	set_linear_damp(0.9)
@@ -42,9 +67,15 @@ func floaty_controls(delta):
 		apply_impulse(Vector2(), Vector2(-strafe_force, 0))
 	if Input.is_action_pressed("ui_right"):
 		apply_impulse(Vector2(), Vector2(strafe_force, 0))
+	
+	set_view_direction()
 
-func _input(event):
-	if event.is_action_released("ui_left") and is_colliding():
-		set_linear_velocity(Vector2())
-	if event.is_action_released("ui_right") and is_colliding():
-		set_linear_velocity(Vector2())
+func floory_controls(delta):
+	if Input.is_action_pressed("ui_left"):
+		apply_impulse(Vector2(), Vector2(-0.05 * (get_linear_velocity().x+run_speed), 0))
+		get_node("Sprite").set_scale(0.4 * Vector2(-1, 1))
+	elif Input.is_action_pressed("ui_right"):
+		apply_impulse(Vector2(), Vector2(-0.05 * (get_linear_velocity().x-run_speed), 0))
+		get_node("Sprite").set_scale(0.4 * Vector2(1, 1))
+	elif !Input.is_action_pressed("jump"):
+		set_linear_damp(10.0)
